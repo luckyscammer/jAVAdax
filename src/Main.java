7fa3a792129d3bfacfcbb20bf2910eb9ae327847
Pliffdax <stepanov-oleksandr@ukr.net>
2024-03-15 23:27:14 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int groupNumber = 3525;
 
-        int C5 = groupNumber % 5;
-        int C7 = groupNumber % 7;
-        int C11 = groupNumber % 11;
+        int C3 = groupNumber % 3;
+        int C13 = groupNumber % 17;
 
-        System.out.println("C5: " + C5);
-        System.out.println("C7: " + C7);
-        System.out.println("C11: " + C11);
+        System.out.println("C5: " + C3);
+        System.out.println("C7: " + C13);
     }
 }
