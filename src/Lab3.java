956f4fda1129ecbad48fe056561489bbd484e4c0
Pliffdax <stepanov-oleksandr@ukr.net>
2024-03-16 18:31:13 +0200

    Before changes

--- src/Lab3.java
+++ src/Lab3.java
@@ -1,25 +1,58 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
+@SuppressWarnings("StringBufferReplaceableByString")
 public class Lab3 {
+    public static String vowelsToCount = "aeiouyAEIOUYаеєиіїоуюяАЕЄИІЇОУЮЯ";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String text;
         while (true) {
             System.out.print("Будь ласка, введіть рядок (введення повинне містити букви): ");
-            text = scanner.nextLine();
+            String input = scanner.nextLine();
+            StringBuilder textBuilder = new StringBuilder(input);
 
-            if (text.isEmpty() || text.matches("\\d+")) {
+            if (input.isEmpty() || input.matches("\\d+") || !input.matches(".*[" + Lab3.vowelsToCount + "].*")) {
                 System.out.println("Введення не відповідає умовам, спробуйте знову.");
+                textBuilder.setLength(0);
             } else {
-                System.out.println("Ви ввели відповідний рядок: " + text);
+                System.out.println("Ви ввели відповідний рядок: " + textBuilder);
+                scanner.close();
+                StringBuilder noSymbolsBuilder = deleteSymbols(textBuilder);
+                System.out.println("Відредагований текст без зайвих символів: " + noSymbolsBuilder);
+                String[] textBuilderWords = noSymbolsBuilder.toString().split("\\s+");
+                Arrays.sort(textBuilderWords, (firstWord, secondWord) -> {
+                    int vowelsCount1 = countVowels(firstWord);
+                    int vowelsCount2 = countVowels(secondWord);
+                    if (vowelsCount1 != vowelsCount2) {
+                        return vowelsCount1 - vowelsCount2;
+                    } else {
+                        return firstWord.length() - secondWord.length();
+                    }
+                });
+                String resultString = String.join(" ", textBuilderWords);
+                StringBuilder finishedTextBuilder = new StringBuilder(resultString);
+                System.out.println("Відсортований текст за зростянням кількості голосних у слові: " + finishedTextBuilder);
                 break;
             }
         }
-        scanner.close();
+    }
 
-        System.out.println(text);
-        String[] eachWordArray = text.split(" ");
-        System.out.println(Arrays.toString(eachWordArray));
+    static StringBuilder deleteSymbols(StringBuilder textBuilder) {
+        for (int i = textBuilder.length() - 1; i >= 0; --i) {
+            if (!Character.isLetter(textBuilder.charAt(i)) && textBuilder.charAt(i) != ' ') {
+                textBuilder.deleteCharAt(i);
+            }
+        }
+        return textBuilder;
+    }
+
+    static int countVowels(String word) {
+        int count = 0;
+        for (int i = 0; i <= word.length() - 1; ++i) {
+            if (Lab3.vowelsToCount.indexOf(word.charAt(i)) != -1) {
+                ++count;
+            }
+        }
+        return count;
     }
 }
